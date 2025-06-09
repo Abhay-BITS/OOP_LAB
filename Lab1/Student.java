56109628213ac10d7b2b224651087e8de7ebe537
Abhay Kumar Maddheshiya <abhaymaddheshiya1066@gmail.com>
2025-06-10 01:52:16 +0530

    Update Student.java

--- Lab1/Student.java
+++ Lab1/Student.java
@@ -1,18 +1,26 @@
 //Write a student class and a main to create some students and irerate through them, we will also use a basic constructor to ease our job
 //class name in capital
 
-package Lab2;
-
 public class Student {
     String name;
     String ID;
-    Double CGPA;
+    double CGPA;
     String Campus;
-    public Student(String n, String i, double c, String t){  // constructor to do smart work
-        this.name = n;
-        this.ID = i;
-        this.CGPA = c;
-        this.Campus = t;
+
+    // Constructor
+    public Student(String name, String ID, double CGPA) {
+        this.name = name;
+        this.ID = ID;
+        this.CGPA = CGPA;
+        this.Campus = assignCampus(ID);
+    }
+
+    // Method to assign campus from last char of ID
+    private String assignCampus(String id) {
+        char lastChar = Character.toUpperCase(id.charAt(id.length() - 1));
+        if (lastChar == 'H') return "Hyd";
+        else if (lastChar == 'P') return "Pilani";
+        else if (lastChar == 'G') return "Goa";
+        else return "Unknown";
     }
 }
-
