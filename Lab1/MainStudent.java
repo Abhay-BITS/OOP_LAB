1e31945614331afbf2db1806f4d7ee208886f95d
Abhay Kumar Maddheshiya <abhaymaddheshiya1066@gmail.com>
2025-06-10 01:51:56 +0530

    Update MainStudent.java

--- Lab1/MainStudent.java
+++ Lab1/MainStudent.java
@@ -1,45 +1,59 @@
-package Lab2;
-
 public class MainStudent {
 
     public static void main(String args[]) {
-        // Create an array of 6 Student objects and initialize them with name, ID, CGPA, and campus
         Student students[] = new Student[6];
-        students[0] = new Student("Simran", "2020PS1934H", 8.65, "Pilani");
-        students[1] = new Student("Raj", "2020PS8695H", 8.24, "Hyd");
-        students[2] = new Student("Rahul", "2020PS6932P", 7.21, "GOA");
-        students[3] = new Student("Soniya", "2020PS1748P", 9.2, "GOA");
-        students[4] = new Student("Veer", "2020PS3874G", 8.7, "HYD");
-        students[5] = new Student("Zaara", "2020PS3225G", 8.7, "pilani");
+        students[0] = new Student("Simran", "2020PS1934H", 8.65);
+        students[1] = new Student("Raj", "2020PS8695H", 8.24);
+        students[2] = new Student("Rahul", "2020PS6932P", 7.21);
+        students[3] = new Student("Soniya", "2020PS1748P", 9.2);
+        students[4] = new Student("Veer", "2020PS3874G", 8.7);
+        students[5] = new Student("Zaara", "2020PS3225G", 8.7);
 
-        // Sort students by CGPA in ascending order ("A" = Ascending, "D" = Descending)
-        sort(students, "A");
+        // Example: sort by name in ascending order
+        sort(students, "Campus", "A");
 
-        // Display the sorted student data in tabular format
         System.out.println("ID\t\tName\t\tCGPA\t\tCampus");
         for (Student s : students) {
             System.out.printf("%s\t%s\t\t%.2f\t\t%s\n", s.ID, s.name, s.CGPA, s.Campus);
         }
     }
 
-    /
-      Sorts the student array based on CGPA using Bubble Sort.
-      @param students The array of Student objects to sort.
-      @param direction "A" for ascending, "D" for descending order of CGPA.
-     /
-    public static void sort(Student[] students, String direction) {
-        // Bubble Sort logic
+    public static void sort(Student[] students, String key, String direction) {
         for (int i = 0; i < students.length - 1; i++) {
             for (int j = 0; j < students.length - i - 1; j++) {
+                boolean shouldSwap = false;
 
-                // If sorting in ascending order and current CGPA is greater than next, swap them
-                if (direction.equalsIgnoreCase("A") && students[j].CGPA > students[j + 1].CGPA) {
-                    Student temp = students[j];
-                    students[j] = students[j + 1];
-                    students[j + 1] = temp;
+                switch (key.toLowerCase()) {
+                    case "cgpa":
+                        if (direction.equalsIgnoreCase("A"))
+                            shouldSwap = students[j].CGPA > students[j + 1].CGPA;
+                        else
+                            shouldSwap = students[j].CGPA < students[j + 1].CGPA;
+                        break;
 
-                // If sorting in descending order and current CGPA is less than next, swap them
-                } else if (direction.equalsIgnoreCase("D") && students[j].CGPA < students[j + 1].CGPA) {
+                    case "id":
+                        if (direction.equalsIgnoreCase("A"))
+                            shouldSwap = students[j].ID.compareToIgnoreCase(students[j + 1].ID) > 0;
+                        else
+                            shouldSwap = students[j].ID.compareToIgnoreCase(students[j + 1].ID) < 0;
+                        break;
+
+                    case "name":
+                        if (direction.equalsIgnoreCase("A"))
+                            shouldSwap = students[j].name.compareToIgnoreCase(students[j + 1].name) > 0;
+                        else
+                            shouldSwap = students[j].name.compareToIgnoreCase(students[j + 1].name) < 0;
+                        break;
+
+                    case "campus":
+                        if (direction.equalsIgnoreCase("A"))
+                            shouldSwap = students[j].Campus.compareToIgnoreCase(students[j + 1].Campus) > 0;
+                        else
+                            shouldSwap = students[j].Campus.compareToIgnoreCase(students[j + 1].Campus) < 0;
+                        break;
+                }
+
+                if (shouldSwap) {
                     Student temp = students[j];
                     students[j] = students[j + 1];
                     students[j + 1] = temp;
